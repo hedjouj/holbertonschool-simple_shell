3890e7535df8e9b437c50f303d8ad61d2e0a626a
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-04-18 14:13:47 +0000

    Fix : Modify the command file and adding function to find path

---
+++ command.c
@@ -0,0 +1,96 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+char *find_path(char *command)
+{
+	char *path_env, *path_copy, *dir, *full_path;
+	struct stat st;
+
+	if (command == NULL)
+		return (NULL);
+
+	if (stat(command, &st) == 0)
+		return (strdup(command));
+
+	path_env = getenv("PATH");
+	if (!path_env)
+		return (NULL);
+
+	path_copy = strdup(path_env);
+	if (!path_copy)
+		return (NULL);
+
+	dir = strtok(path_copy, ":");
+	while (dir != NULL)
+	{
+		full_path = malloc(strlen(dir) + strlen(command) + 2);
+		if (!full_path)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+
+		sprintf(full_path, "%s/%s", dir, command);
+		if (stat(full_path, &st) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
+}	
+
+
+
+/**
+ * execute_command - runs the command*/
+
+int execute_command(char **args)
+{
+	pid_t pid;
+	int status;
+	char *cmd_path = NULL;
+
+	if (args[0] == NULL)
+		return (1);
+
+	/* if it's an absolute or relative path */
+	if (access(args[0], X_OK) == 0)
+		cmd_path = strdup(args[0]);
+	else
+		cmd_path = find_path(args[0]);
+
+	if (!cmd_path)
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		return (127);
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execv(cmd_path, args);
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		exit(127);
+	}
+	else if (pid < 0)
+	{
+		perror("fork");
+		free(cmd_path);
+		return (1);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		free(cmd_path);
+		return (WIFEXITED(status)) ? WEXITSTATUS(status) : 1;
+	}
+}
--- tools.c
+++ tools.c
@@ -1,8 +1,10 @@
 #include "shell.h"
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-
-
+extern char **environ;
 
 void display_prompt(void)
 {
@@ -39,4 +41,35 @@ char **split_line(char *line)
 	tokens[i] = NULL;
 
 	return (tokens);
+}
+
+int handle_builtin(char **args)
+{
+	int i;
+
+	if (args[0] == NULL)
+		return (0);
+
+	if (strcmp(args[0], "exit") == 0)
+	{
+		free(args);
+		exit(0);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		for (i = 0; environ[i]; i++)
+			printf("%s\n", environ[i]);
+		return (1);
+	}
+	else if (strcmp(args[0], "pwd") == 0)
+	{
+		char cwd[1024];
+		if (getcwd(cwd, sizeof(cwd)) != NULL)
+			printf("%s\n", cwd);
+		else
+			perror("getcwd");
+		return (1);
+	}
+
+	return (0); /* Not a builtin */
 }
