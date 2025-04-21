c9f97c08db2308c9d0863acb837841a26378fbb0
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-04-21 14:25:32 +0000

    FIX : Big problem of merging and resolving it

---
+++ tools.c
@@ -0,0 +1,80 @@
+#include "shell.h"
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+void display_prompt(void)
+{
+	printf("$ ");
+	fflush(stdout); /* Force the immediat displaying of the prompt */
+}
+
+/*
+ *
+ *
+ *
+ *
+ */
+
+char **split_line(char *line)
+
+{
+	int bufsize = 64; 
+	int i = 0;
+	char **tokens = malloc(sizeof(char *) * bufsize);
+	char *token;
+
+	if (!tokens)
+	{
+		perror("allocation");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(line, " \t\r\n");
+	while (token != NULL)
+	{
+		tokens[i++] = token;
+		token = strtok(NULL, " \t\r\n");
+	}
+	tokens[i] = NULL;
+
+	return (tokens);
+}
+
+int handle_builtin(char **args)
+{
+	if (args[0] == NULL)
+		return (-1);
+
+	if (strcmp(args[0], "exit") == 0)
+	{
+		free(args);
+		exit(0);/* We leave without error*/
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		printenv();
+		return(0); /* Return 0 for success*/
+	}
+	else if (strcmp(args[0], "pwd") == 0)
+	{
+		char cwd[1024];
+		if (getcwd(cwd, sizeof(cwd)) != NULL)
+			printf("%s\n", cwd);
+		else
+			perror("getcwd");
+		return (1); /*Error*/
+	}
+
+	return (-1); /* Not a builtin */
+}
+
+void printenv(void)
+{
+	int i;
+
+	for (i = 0; environ[i]; i++)
+		printf("%s\n", environ[i]);
+}
